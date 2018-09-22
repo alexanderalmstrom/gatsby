a05c57a70ce9803104031193e2349b1acadb35e2
Alexander Almström <info@alexanderalmstrom.com>
2018-09-22 20:58:44 +0200

    Updated readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'Alexander Almström',
-    description: 'This is cool'
+    description: 'This is so cool'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
+        name: 'gatsby-site',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#000000',
+        theme_color: '#000000',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png',
       },
