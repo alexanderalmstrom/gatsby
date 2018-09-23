62d09f65e34a97d637db0b715a37bb4fe2d47057
Alexander Almström <info@alexanderalmstrom.com>
2018-09-23 02:43:42 +0200

    Convert to tabs

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,42 +2,42 @@ const Promise = require('bluebird')
 const path = require('path')
 
 exports.createPages = ({ graphql, getNode, actions }) => {
-  const { createPage } = actions
+	const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    const postTemplate = path.resolve('./src/templates/post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulPost {
-              edges {
-                node {
-                  name
-                  slug
-                }
-              }
-            }
-          }
-          `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
+	return new Promise((resolve, reject) => {
+		const postTemplate = path.resolve('./src/templates/post.js')
+		resolve(
+			graphql(
+				`
+					{
+						allContentfulPost {
+							edges {
+								node {
+									name
+									slug
+								}
+							}
+						}
+					}
+				`
+			).then(result => {
+				if (result.errors) {
+					console.log(result.errors)
+					reject(result.errors)
+				}
 
-        const posts = result.data.allContentfulPost.edges
+				const posts = result.data.allContentfulPost.edges
 
-        posts.forEach((post, index) => {
-          createPage({
-            path: `/article/${post.node.slug}/`,
-            component: postTemplate,
-            context: {
-              slug: post.node.slug
-            },
-          })
-        })
-      })
-    )
-  })
+				posts.forEach((post, index) => {
+					createPage({
+						path: `/article/${post.node.slug}/`,
+						component: postTemplate,
+						context: {
+							slug: post.node.slug
+						},
+					})
+				})
+			})
+		)
+	})
 }
